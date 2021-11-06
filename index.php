e771df01499060da57e21b2b7a801308a87b205d
arippe <aliareef17@gmail.com>
2021-11-06 20:49:12 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Graduate Maldives</title>
+
+    <style type="text/tailwindcss">
+        .wrapper{
+            @apply max-w-6xl px-2 mx-auto items-center; 
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="h-screen w-full bg-no-repeat bg-cover"
+        style="background-image:linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(graduate-students.jpg)">
+        <div class="z-10 text-white">
+            <div class="bg-primary w-full">
+                <nav class="wrapper flex justify-between">
+                    <div class="flex space-x-7">
+                        <div class="relative p-1 h-12 curser-pointer">
+                            <img class="h-full" src="Logo.svg" alt="">
+                        </div>
+
+                        <ul class="hidden md:flex items-center space-x-4">
+                            <li><a class="font-semibold" href="#">Home</a></li>
+                            <li><a class="font-semibold" href="#">Institutions</a></li>
+                            <li><a class="font-semibold" href="#">Courses</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <a class="font-semibold text-secondary" href="#">Login/Signup</a>
+                    </div>
+                </nav>
+            </div>
+
+            <div class="wrapper flex h-screen -mt-14 justify-center items-center">
+                <div class="w-full">
+                    <h1 class="text-3xl md:text-5xl text-center font-semibold">Welcome to Graduate Maldives</h1>
+                    <h2 class="text-center p-8 text-2xl md:text-3xl text-gray-300">Find the best Course suited for you
+                    </h2>
+
+                    <form class="mt-8 flex flex-wrap md:space-y-0 space-y-4 md:space-x-2 md:justify-between" action="">
+
+                        <div class="relative w-full md:w-1/4 h-10">
+                            <input id="emailf" name="email" type="text"
+                                class="rounded peer h-full w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600"
+                                placeholder="email" />
+                            <label for="emailf"
+                                class="absolute left-2 text-lg transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-8 peer-focus:text-white peer-focus:font-semibold peer-focus:text-lg">Email
+                                address</label>
+                        </div>
+
+                        <div class="relative w-full md:w-1/4 h-10">
+                            <input id="emailf" name="email" type="text"
+                                class="rounded peer h-full w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600"
+                                placeholder="email" />
+                            <label for="emailf"
+                                class="absolute left-2 text-lg transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-8 peer-focus:text-white peer-focus:font-semibold peer-focus:text-lg">Email
+                                address</label>
+                        </div>
+
+                        <div class="relative w-full md:w-1/4 h-10">
+                            <input id="emailf" name="email" type="text"
+                                class="rounded peer h-full w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600"
+                                placeholder="email" />
+                            <label for="emailf"
+                                class="absolute left-2 text-lg transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-8 peer-focus:text-white peer-focus:font-semibold peer-focus:text-lg">Email
+                                address</label>
+                        </div>
+
+                        <button
+                            class="rounded w-full h-10 text-center font-semibold text-lg md:w-36 bg-secondary">Search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <main class="bg-gradient-to-r from-indigo-50 to-indigo-100">
+        <div class="wrapper py-24">
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-4">
+                <div class="bg-white shadow-lg rounded-md p-2">
+                    <h1 class="text-secondary font-semibold text-xl">Bachelor of Computer Science</h1>
+                    <div class="flex mb-4 items-center space-x-3">
+                        <img class="h-24" src="mnu.png" alt="">
+                        <div>
+                            <h1 class="my-3 font-semibold text-gray-500">Maldives National University</h1>
+                            <div class="flex space-x-2 text-gray-500 mb-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                    stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
+                                </svg>
+                                <p>ffe</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="flex space-x-2 text-gray-500 mb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <p>ffe</p>
+                    </div>
+                    <div class="flex space-x-2 text-gray-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <p>ffe</p>
+                    </div>
+                </div>
+                <div
+                    class="bg-white shadow-lg h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">
+                    2</div>
+                <div
+                    class="bg-white shadow-lg h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">
+                    3</div>
+                <div
+                    class="bg-white shadow-lg  h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">
+                    4</div>
+                <div
+                    class="bg-white shadow-lg  h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">
+                    5</div>
+                <div
+                    class="bg-white shadow-lg  h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold">
+                    6</div>
+
+            </div>
+        </div>
+    </main>
+
+
+    <script src="https://cdn-tailwindcss.vercel.app"></script>
+    <script src="tailwind-config.js"></script>
+</body>
+
+</html>
---
+++ tailwind-config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        container: {
+            padding: '4rem',
+            center: true,
+        },
+        extend: {
+            colors: {
+                primary: '#273044',
+                secondary: '#eb6260'
+            }
+        }
+    }
+}
