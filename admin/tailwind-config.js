8fe904641bd77a1166cea6a03ec9b7457814a3cb
arippe <aliareef17@gmail.com>
2021-11-11 22:00:33 +0500

    added admin pages

---
+++ admin/tailwind-config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+        container: {
+            padding: '4rem',
+            center: true,
+        },
+        extend: {
+            colors: {
+                primary: '#273044',
+                secondary: '#eb6260',
+                c_pink: '#ee0979',
+                c_orange: '#ff6a00',
+                c_green: '#00b09b',
+                c_lightgreen: '#96c93d',
+            }
+        }
+    }
+}
